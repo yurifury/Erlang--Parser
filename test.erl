8b07114026e6f4a684400792297d3bb85fe121fd
Anneli Cuss <celtic@sairyx.org>
2011-09-11 16:10:21 +1000

    Parsing binary operators reveals where no precedence causes issues

--- test.erl
+++ test.erl
@@ -1,7 +1,8 @@
--module(test).
+%-module(test).
+%-export([test/0]).
 
 test() ->
-    io:format("This is a ~p~n.", ["test"]),
+    %io:format("This is a ~p~n.", ["test"]),
+    9/2,
     42.
 
-ok.
