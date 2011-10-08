d60e3521cbcaa730376bb018bc21fbbbdc5cf08f
Anneli Cuss <celtic@sairyx.org>
2011-10-08 15:26:55 +1100

    Parsing/lexing with Perl cont'd

--- erlparse.pl
+++ erlparse.pl
@@ -2,53 +2,80 @@ use strict;
 use warnings;
 use 5.010;
 
+use Data::Dumper;
 use Parse::Lex;
+use Parser;
 
-my $lexer;
+our $lexer;
+our $lexer_string;
+our $lexer_skip;
+
+sub skip_token { $lexer_skip = 1; }
+
 my @token = (
     'EXTCALL',		q!([[:alpha:]_]+):([[:alpha:]_]+)\(!,
     'INTCALL',		q!([[:alpha:]_]+)\(!,
     'ATOM',		q![[:alpha:]_]+!,
     'INTEGER',		q![[:digit:]]+!,
     'DIRECTIVE',	q!-([a-z]+)\(!,
-    'STRINGSTART',	q!"!, sub {
-	$lexer->start('string');
+
+    'dqstr:LIT',	q!\\\\"!, sub {
+	$lexer_string .= substr($_[1], 1);
+	skip_token;
     },
-    'string:LIT',	q!\\.!,
-    'string:END',       q!"!, sub {
-	$lexer->end('string');
+    'dqstr:STRING',       q!"!, sub {
+	$lexer->end('dqstr');
+	$lexer_string;
     },
-    'string:CONTENT',	q![^"]+!,
+    'dqstr:CONTENT',	q![^"\\\\]+!, sub {
+	$lexer_string .= $_[1];
+	skip_token;
+    },
+    'OPENSTRING',	q!"!, sub {
+	$lexer->start('dqstr');
+	$lexer_string = '';
+	skip_token;
+    },
+
+    'WHITESPACE',	q!\s!, sub { skip_token },
+    'COMMENT',		q!%.*!, sub { skip_token },
+
     'LPAREN',		q!\(!,
     'RPAREN',		q!\)!,
     'PERIOD',		q!\.!,
     'RARROW',		q!->!,
-    'WHITESPACE',	q!\s!,
-    'COMMENT',		q!%.*!,
     'LISTOPEN',		q!\[!,
     'LISTCLOSE',	q!\]!,
     'DIV',		q!/!,
     'COMMA',		q!,!,
-    'ERROR',		'.*', sub {
-	die qq!can't analyse: "$_[1]"!;
-    }
+    'ERROR',		'.*', sub { die qq!can't analyse: "$_[1]"! },
+    'SKIP',		q!!,
 );
 
-Parse::Lex->trace;
-Parse::Lex->exclusive('string');
+Parse::Lex->exclusive('dqstr');
 $lexer = Parse::Lex->new(@token);
 $lexer->from(\*DATA);
 
-TOKEN:while (1) {
-    my $token = $lexer->next;
-    if (not $lexer->eoi) {
-	print "Line $.\t";
-	print "Type: ", $token->name, "\t";
-	print "Content:->", $token->text, "<-\n";
-    } else {
-	last TOKEN;
+sub Lex {
+    my $token;
+    $lexer_skip = 0;
+    LOOP:while (1) {
+	$token = $lexer->next;
+
+	if ($lexer->eoi or not $token) {
+	    return ('', undef);
+	} elsif (not $lexer_skip) {
+	    last LOOP;
+	}
+	$lexer_skip = 0;
     }
+
+    say "Line $.\t", $token->name, "\t", $token->text;
+    return ($token->name, $token->text);
 }
+
+my $parser = new Parser;
+my $result = $parser->YYParse(yylex => \&Lex);
 
 __END__
 -module(test).
