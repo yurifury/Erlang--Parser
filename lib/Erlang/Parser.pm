bc4e6376aa47827812d02d62721872692b8912b6
Anneli Cuss <celtic@sairyx.org>
2011-10-09 11:21:46 +1100

    Extending the POD

--- lib/Erlang/Parser.pm
+++ lib/Erlang/Parser.pm
@@ -27,11 +27,50 @@ sub print_tree {
     Erlang::Parser::Dumper->print_tree($fh, $tree);
 }
 
-__END__
+1;
+
+=pod
 
 =head1 NAME
 
 Erlang::Parser - Erlang source code parser
+
+=head1 SYNOPSIS
+
+    use Erlang::Parser;
+
+    my $tree = Erlang::Parser->parse(\*DATA);
+
+=head1 DESCRIPTION
+
+L<Erlang::Parser> is an Erlang source code parser.  You can feed C<parse()> any
+fragment of code which would be acceptable at the top-level of a C<.erl> file,
+including a full file.
+
+=head1 SUPPORT
+
+The source code repository can be found at
+L<http://github.com/anneli/Erlang--Parser>.  If you find any bugs, please open
+an L<issue|http://github.com/anneli/Erlang--Parser/issues>, or send a pull
+request with your fix!
+
+A mailing list is available at
+L<http://groups.google.com/group/erlang--parser-devel>, or you can mail direct
+to L<mailto:erlang--parser-devel@googlegroups.com>.
+
+The author can also be found on L<Twitter|http://twitter.com/unnali>.
+
+=head1 AUTHOR
+
+Anneli Cuss <anneli@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Anneli Cuss.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
 
 =cut
 
