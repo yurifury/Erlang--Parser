fe7722755faf48b4584beb2feca7e266f5593475
Anneli Cuss <celtic@sairyx.org>
2011-10-09 12:07:33 +1100

    Using Module::Starter base for the module.

--- lib/Erlang/Parser.pm
+++ lib/Erlang/Parser.pm
@@ -8,8 +8,6 @@ use strict;
 use warnings;
 use 5.014;
 
-our $VERSION = '0.1';
-
 use Erlang::Parser::Dumper;
 use Erlang::Parser::Lexer;
 use Erlang::Parser::Parser;
@@ -27,19 +25,24 @@ sub print_tree {
     Erlang::Parser::Dumper->print_tree($fh, $tree);
 }
 
-1;
-
-=pod
-
 =head1 NAME
 
 Erlang::Parser - Erlang source code parser
+
+=head1 VERSION
+
+Version 0.1
+
+=cut
+
+our $VERSION = '0.1';
 
 =head1 SYNOPSIS
 
     use Erlang::Parser;
 
     my $tree = Erlang::Parser->parse(\*DATA);
+    Erlang::Parser->print_tree(*STDOUT, $tree);
 
 =head1 DESCRIPTION
 
@@ -47,31 +50,50 @@ L<Erlang::Parser> is an Erlang source code parser.  You can feed C<parse()> any
 fragment of code which would be acceptable at the top-level of a C<.erl> file,
 including a full file.
 
-=head1 SUPPORT
-
-The source code repository can be found at
-L<http://github.com/anneli/Erlang--Parser>.  If you find any bugs, please open
-an L<issue|http://github.com/anneli/Erlang--Parser/issues>, or send a pull
-request with your fix!
-
-A mailing list is available at
-L<http://groups.google.com/group/erlang--parser-devel>, or you can mail direct
-to <erlang--parser-devel@googlegroups.com>.
-
-The author can also be found on L<Twitter|http://twitter.com/unnali>.
-
 =head1 AUTHOR
 
-Anneli Cuss <anneli@cpan.org>
+Anneli Cuss, C<< <anneli at cpan.org> >>
 
-=head1 COPYRIGHT AND LICENSE
+=head1 SUPPORT
 
-This software is copyright (c) 2011 by Anneli Cuss.
+You can find documentation for L<Erlang::Parser> with the perldoc command.
 
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
+    perldoc Erlang::Parser
 
+Other places of interest:
+
+=over 4
+
+=item * GitHub: source code repository
+
+L<http://github.com/anneli/Erlang--Parser>
+
+=item * GitHub: open an issue
+
+L<http://github.com/anneli/Erlang--Parser/issues>
+
+=item * Mailing list
+
+L<http://groups.google.com/group/erlang--parser-devel>, <erlang--parser-devel@googlegroups.com>
+
+=item * Twitter: the author
+
+L<http://twitter.com/unnali>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Anneli Cuss.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
 
 =cut
+
+1;
 
 # vim: set sw=4:
